f2960996a8789e0ac0e6e0f901a065f05933ea44
Abner Carleto <abnercarleto@gmail.com>
2021-11-27 00:55:39 -0300

    Controller for create token and set it to email

--- api/app/controllers/api/v1/auth_controller.rb
+++ api/app/controllers/api/v1/auth_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AuthController < ApplicationController
   def generate_token
-    Identity::CreateToken.
-      call(email: params.require(:email)).
+    Identity::GenerateToken.
+      call(email: params.require(:email), permit_regenerate: params.fetch(:permit_regenerate, false)).
       on_success { |result| render json: result.data }.
       on_failure { |result| render json: result.data, status: :unprocessable_entity }
   end
--- api/spec/requests/api/v1/auth_spec.rb
+++ api/spec/requests/api/v1/auth_spec.rb
@@ -1,7 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Auths", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  describe "POST /api/v1/auth/token" do
+    let(:email) { "user-#{SecureRandom.hex(5)}@email.com" }
+    let(:json_body) { JSON.parse(response.body) }
+
+    context 'when user is new' do
+      it 'create token for new user' do
+        post '/api/v1/auth/token', params: { email: email, format: :json }
+
+        expect(response).to have_http_status(:ok)
+        expect(json_body).to eq({ 'sent_to' => email })
+      end
+    end
+
+    context 'when user is persisted' do
+      let!(:identity_user) { create(:identity_user, email: email) }
+
+      context 'and not permit regenerate token' do
+        it 'not create token for persisted user' do
+          post '/api/v1/auth/token', params: { email: email, format: :json }
+
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(json_body).to eq({ 'token_already_generated' => true })
+        end
+      end
+
+      context 'and permit regenerate token' do
+        it 'create token for persisted user' do
+          post '/api/v1/auth/token', params: { email: email, permit_regenerate: true, format: :json }
+
+          expect(response).to have_http_status(:ok)
+          expect(json_body).to eq({ 'sent_to' => email })
+        end
+      end
+    end
+
+    context 'when email is invalid' do
+      let(:email) { "user-#{SecureRandom.hex(5)}.com" }
+
+      it 'return email validation error' do
+        post '/api/v1/auth/token', params: { email: email, format: :json }
+
+          expect(response).to have_http_status(:unprocessable_entity)
+          expect(json_body).to eq({ 'errors' => { 'email' => ['is invalid'] } })
+      end
+    end
   end
 end
